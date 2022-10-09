972c16f33d490deb5d273da2d9014cf00035f5f4
csfrez <csfrez@163.com>
2022-10-09 14:39:34 +0800

    删除无用代码

--- mybatis-plus-datalimit/src/main/java/com/csfrez/datalimit/config/DatalimitConfig.java
+++ mybatis-plus-datalimit/src/main/java/com/csfrez/datalimit/config/DatalimitConfig.java
@@ -3,39 +3,22 @@ package com.csfrez.datalimit.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import com.csfrez.datalimit.databind.IJsonBindStrategy;
 import com.csfrez.datalimit.datascope.IDataScopeProvider;
 import com.csfrez.datalimit.intercept.DatalimitIntercept;
-import com.csfrez.datalimit.license.IGrantSupplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
 
 
 @Lazy
-@Configuration(
-        proxyBeanMethods = false
-)
+@Configuration(proxyBeanMethods = false)
 public class DatalimitConfig {
 
-    @Autowired(
-            required = false
-    )
+    @Autowired(required = false)
     private IDataScopeProvider dataScopeProvider;
-
-    @Autowired(
-            required = false
-    )
-    private IJsonBindStrategy jsonBindStrategy;
-
-    @Autowired(
-            required = false
-    )
-    private IGrantSupplier grantSupplier;
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
--- mybatis-plus-datalimit/src/main/java/com/csfrez/datalimit/license/IGrantSupplier.java
+++
@@ -1,7 +0,0 @@
-package com.csfrez.datalimit.license;
-
-public interface IGrantSupplier {
-
-    String get(String var1);
-
-}
